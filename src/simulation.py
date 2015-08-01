1daec3417464b9754ec110aebcba27e0d56d2a37
leovt <leonhard.vogt@gmx.ch>
2015-08-01 18:34:49 +0200

    new path_graph for managing paths and connectivity

--- src/simulation.py
+++ src/simulation.py
@@ -5,6 +5,7 @@ Created on 8 Jul 2015
 '''
 
 import enum
+import path_graph
 @enum.unique
 class Terrain(enum.Enum):
     GRASS = 1
@@ -56,59 +57,80 @@ class Person:
         self.action = 'wait'
         self.pose = 'stand'
         self.action_started = 0
-        self.target = (x, y)
-        self.waypoints = []
+        self.target = None
+        self.next_waypoint = None
+        self.last = None
         self.direction = 0
-        self.speed = 0.35
+        self.speed = 0.45
 
     def update(self, t, dt):
         if self.action == 'wait':
-            if self.waypoints:
-                logging.debug('Person {} is at {:.1f},{:.1f} and plans to go {}'.format(self.name, self.x, self.y, [(t.column, t.row) for t in self.waypoints]))
-                nextwp = self.waypoints.pop()
-                self.target = (nextwp.column + 0.5, nextwp.row + 0.5)
+            if t >= self.action_started + 1.0:
+                self.target = None
+                self.next_waypoint = None
+                self.last = None
+
                 self.action = 'walk'
                 self.action_started = t
-            elif t >= self.action_started + 1.0:
-                self.action = 'walk'
-                self.action_started = t
-                x, y = (random.uniform(0, 10), random.uniform(0, 10))
-                logging.debug('Person {} has new target {:.1f},{:.1f}'.format(self.name, x, y))
-                self.waypoints = self.simu.find_path(int(self.x), int(self.y), int(x), int(y))
-                if self.waypoints is None:
-                    logging.debug('Impossible to go there')
-                    self.waypoints = []
-                logging.debug('Person {} is at {:.1f},{:.1f} and plans to go {}'.format(self.name, self.x, self.y, [(t.column, t.row) for t in self.waypoints]))
+
+
         elif self.action == 'walk':
-            if self.target[0] < self.x:
+            if self.next_waypoint is None:
+                if self.target:
+                    assert False, 'Not yet implemented'
+                else:
+                    X = int(self.x)
+                    Y = int(self.y)
+                    p = self.simu.path_graph.path.get((X, Y, 0))
+                    if p is None:
+                        # we are not on a path
+                        self.action = 'wait'
+                        self.action_started = t
+                        return
+                    if not p.neighbours:
+                        # we are on an isolated path
+                        self.action = 'wait'
+                        self.action_started = t
+                        return
+                    nbs = [nb.pos for nb in p.neighbours]
+                    if self.last in nbs and len(nbs) > 1:
+                        nbs.remove(self.last)
+                    self.last = p.pos
+                    nb = random.choice(nbs)
+                    if nb[0] == X:
+                        self.next_waypoint = (self.x, nb[1] + random.uniform(0.2, 0.8))
+                    else:
+                        assert nb[1] == Y
+                        self.next_waypoint = (nb[0] + random.uniform(0.2, 0.8), self.y)
+
+            if self.next_waypoint[0] < self.x:
                 self.x -= self.speed * dt
-                self.x = max(self.x, self.target[0])
-            elif self.target[0] > self.x:
+                self.x = max(self.x, self.next_waypoint[0])
+            elif self.next_waypoint[0] > self.x:
                 self.x += self.speed * dt
-                self.x = min(self.x, self.target[0])
-            elif self.target[1] < self.y:
+                self.x = min(self.x, self.next_waypoint[0])
+            elif self.next_waypoint[1] < self.y:
                 self.y -= self.speed * dt
-                self.y = max(self.y, self.target[1])
-            elif self.target[1] > self.y:
+                self.y = max(self.y, self.next_waypoint[1])
+            elif self.next_waypoint[1] > self.y:
                 self.y += self.speed * dt
-                self.y = min(self.y, self.target[1])
+                self.y = min(self.y, self.next_waypoint[1])
             else:
-                self.action = 'wait'
-                self.action_started = t
+                self.next_waypoint = None
         self.update_pose()
 
     def update_pose(self):
-        if self.action == 'wait':
+        if self.action == 'wait' or self.next_waypoint is None:
             self.pose = 'stand'
         elif self.action == 'walk':
             self.pose = 'walk'
-            if self.target[0] < self.x:
+            if self.next_waypoint[0] < self.x:
                 self.direction = 180
-            elif self.target[0] > self.x:
+            elif self.next_waypoint[0] > self.x:
                 self.direction = 0
-            elif self.target[1] < self.y:
+            elif self.next_waypoint[1] < self.y:
                 self.direction = 270
-            elif self.target[1] > self.y:
+            elif self.next_waypoint[1] > self.y:
                 self.direction = 90
         else:
             assert False, 'unknown action ' + self.action
@@ -120,7 +142,7 @@ class Person:
                 'action': self.action,
                 'action_started': self.action_started,
                 'target': self.target,
-                'waypoints': [(t.column, t.row) for t in self.waypoints]}
+                'next_waypoint': self.next_waypoint}
 
     @staticmethod
     def deserialize(simu, data):
@@ -128,7 +150,7 @@ class Person:
         self.action = data['action']
         self.action_started = data['action_started']
         self.target = data['target']
-        self.waypoints = [self.simu.map[c][r] for (c, r) in data['waypoints']]
+        self.waypoints = data['next_waypoint']
         self.update_pose()
         return self
 
@@ -147,46 +169,28 @@ class Simulation:
         self.world_height = world_height
         self.map = [[Tile(Terrain.GRASS, row, col)
                      for row in range(world_height)] for col in range(world_width)]
+        self.path_graph = path_graph.PathGraph()
 
-        for i, col in enumerate([[3, 10, 9], [5, None, 5], [6, 9, 5], [2, 14, 12]]):
-            for j, p in enumerate(col):
-                self.map[i][j].path = p
-
-        self.path_graph = {}
-        self.make_path_graph()
-
-        self.persons = [Person(self, i, i // 3, i % 3) for i in range(10)]
+        self.persons = [Person(self, i, i // 3 + 0.5, i % 3 + 0.5) for i in range(10)]
         self.shops = [Object(x=5, y=4)]
         self.time = 0
 
     def set_path(self, column, row, path):
+        pos = (column, row, 0)
         if path:
-            self.map[column][row].path = 0
-            if column < self.world_width - 1 and self.map[column + 1][row].path is not None:
-                self.map[column][row].path |= 1
-                self.map[column + 1][row].path |= 4
-            if column > 0 and self.map[column - 1][row].path is not None:
-                self.map[column][row].path |= 4
-                self.map[column - 1][row].path |= 1
-            if row < self.world_height - 1 and self.map[column][row + 1].path is not None:
-                self.map[column][row].path |= 2
-                self.map[column][row + 1].path |= 8
-            if row > 0 and self.map[column][row - 1].path is not None:
-                self.map[column][row].path |= 8
-                self.map[column][row - 1].path |= 2
-
+            if pos in self.path_graph.path:
+                return
+            self.path_graph.add_path_element((column, row, 0), path_graph.TYPE_FLAT)
+            self.map[column][row].path = self.path_graph.path[(column, row, 0)].connection_bitfield
+            neighbours = self.path_graph.path[pos].neighbours
         else:
+            if pos not in self.path_graph.path:
+                return
+            neighbours = self.path_graph.path[pos].neighbours
             self.map[column][row].path = None
-            if column < self.world_width - 1 and self.map[column + 1][row].path is not None:
-                self.map[column + 1][row].path &= 11
-            if column > 0 and self.map[column - 1][row].path is not None:
-                self.map[column - 1][row].path &= 14
-            if row < self.world_height - 1 and self.map[column][row + 1].path is not None:
-                self.map[column][row + 1].path &= 7
-            if row > 0 and self.map[column][row - 1].path is not None:
-                self.map[column][row - 1].path &= 13
-        self.make_path_graph()
-
+            self.path_graph.remove_path_element((column, row, 0))
+        for nb in neighbours:
+            self.map[nb.pos[0]][nb.pos[1]].path = nb.connection_bitfield
 
     def update(self, delta_sim_seconds):
         self.time += delta_sim_seconds
@@ -197,60 +201,48 @@ class Simulation:
     def current_datetime(self):
         return get_datetime(self.time)
 
-    def make_path_graph(self):
-        self.path_graph.clear()
-        mask = [1, 2, 4, 8]
-        rmask = [4, 8, 1, 2]
-        NB = [(-1, 0, 2), (0, 1, 1), (1, 0, 0), (0, -1, 3)]
-        for i, col in enumerate(self.map):
-            for j, tile in enumerate(col):
-                neighbour_tiles = [(self.map[i + a][j + b], direction) for (a, b, direction) in NB if 0 <= i + a < self.world_width and 0 <= j + b < self.world_height]
-                if tile.path is None:
-                    self.path_graph[tile] = neighbour_tiles
-                else:
-                    self.path_graph[tile] = [(t, d) for (t, d) in neighbour_tiles if tile.path & mask[d] and t.path is not None and t.path & rmask[d]]
-
     def find_path(self, a, b, i, j):
         start = self.map[a][b]
         goal = self.map[i][j]
-
-        def heuristic_cost_estimate(A, B):
-            return abs(A.row - B.row) + abs(A.column - B.column)
-
-        # A* Algorithm
-        closedset = set()  # The set of nodes already evaluated.
-        openset = {start}  # The set of tentative nodes to be evaluated, initially containing the start node
-        came_from = {}  # The map of navigated nodes.
-
-        g_score = {}
-        g_score[start] = 0  # Cost from start along best known path.
-        # Estimated total cost from start to goal through y.
-        f_score = {}
-        f_score[start] = g_score[start] + heuristic_cost_estimate(start, goal)
-
-        while openset:
-            current = min(openset, key=lambda A:f_score[A])  # the node in openset having the lowest f_score[] value
-            if current is goal:
-                path = []
-                while current != start:
-                    path.append(current)
-                    current = came_from[current]
-                return path
-
-            openset.discard(current)
-            closedset.add(current)
-            for neighbor, _ in self.path_graph[current]:
-                if neighbor in closedset:
-                    continue
-                tentative_g_score = g_score[current] + 1
-
-                if neighbor not in openset or tentative_g_score < g_score[neighbor]:
-                    came_from[neighbor] = current
-                    g_score[neighbor] = tentative_g_score
-                    f_score[neighbor] = g_score[neighbor] + heuristic_cost_estimate(neighbor, goal)
-                    openset.add(neighbor)
-
-        return None  # no path found
+        return None
+#
+#         def heuristic_cost_estimate(A, B):
+#             return abs(A.row - B.row) + abs(A.column - B.column)
+#
+#         # A* Algorithm
+#         closedset = set()  # The set of nodes already evaluated.
+#         openset = {start}  # The set of tentative nodes to be evaluated, initially containing the start node
+#         came_from = {}  # The map of navigated nodes.
+#
+#         g_score = {}
+#         g_score[start] = 0  # Cost from start along best known path.
+#         # Estimated total cost from start to goal through y.
+#         f_score = {}
+#         f_score[start] = g_score[start] + heuristic_cost_estimate(start, goal)
+#
+#         while openset:
+#             current = min(openset, key=lambda A:f_score[A])  # the node in openset having the lowest f_score[] value
+#             if current is goal:
+#                 path = []
+#                 while current != start:
+#                     path.append(current)
+#                     current = came_from[current]
+#                 return path
+#
+#             openset.discard(current)
+#             closedset.add(current)
+#             for neighbor, _ in self.path_graph[current]:
+#                 if neighbor in closedset:
+#                     continue
+#                 tentative_g_score = g_score[current] + 1
+#
+#                 if neighbor not in openset or tentative_g_score < g_score[neighbor]:
+#                     came_from[neighbor] = current
+#                     g_score[neighbor] = tentative_g_score
+#                     f_score[neighbor] = g_score[neighbor] + heuristic_cost_estimate(neighbor, goal)
+#                     openset.add(neighbor)
+#
+#         return None  # no path found
 
     def serialize(self):
         return {'world_width': self.world_width,
@@ -263,10 +255,12 @@ class Simulation:
     def deserialize(data):
         self = Simulation(data['world_width'], data['world_height'])
         self.map = [[Tile.deserialize(col, row, tile_data) for row, tile_data in enumerate(col_data)] for col, col_data in enumerate(data['map'])]
+        for column in self.map:
+            for tile in column:
+                if tile.path is not None:
+                    self.set_path(tile.column, tile.row, True)
         self.persons = [Person.deserialize(self, pers) for pers in data['persons']]
-        self.make_path_graph()
         self.time = data['time']
-        self.make_path_graph()
         return self
 
 
